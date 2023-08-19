723d97501422d4fc68df7bb9acc91a29b815141e
edilson gomes de lima <edskateicevida@gmail.com>
2023-08-19 20:00:25 -0300

    progrmação script

---
+++ fundamentos.js
@@ -0,0 +1,69 @@
+// Fundamentos
+// ava nome = "valor"
+var minhaVariavel = "olá, mundo";
+
+console.log(minhaVariavel);
+
+//comentários
+
+/*
+
+Isso
+Tudo
+
+É
+um Comentário
+
+*/
+// Variáveis e tipos de dados 
+var meuNumero = 10;
+
+console.log(meuNumero);
+
+console.log(meuNumero + 5);
+
+console.log("10" + 5);
+
+console.log(typeof meuNumero);
+
+var boolean = true;
+
+console.log(typeof booleano);
+
+var meuObjeto = {}
+
+// let e const
+// novas formas de declarar variáveis
+let x =10;
+const y = 5;
+
+console.log(typeof x);
+console.log(x, y);
+
+
+// Operadores aritmáticos
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+
+
+// Operadores de comparação.
+console.log(x==y);
+console.log(x !=y);
+
+console.log("5" == 5);
+console.log("5" === 5);
+console.log("5" !== 5);
+
+// Operadores lógicos
+// And &&
+// Or ||
+console.log(10 > 5 && 20> 5);
+console.log(10 > 5 &&  20 <5);
+console.log(10 < 5  && 20 <5);
+
+console.log(10 > 5 || 20 > 5);
+console.log(10 > 5 || 20 < 5);
+console.log(10 < 5 || 20 < 5);
